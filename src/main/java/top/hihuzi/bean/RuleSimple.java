cd08e2752144eadde05463049d5b666424a46e88
hi@hu.zi <hi@hu.zi>
2018-11-06 15:14:27 +0800

    优化 布尔空值处理

--- src/main/java/top/hihuzi/bean/RuleSimple.java
+++ src/main/java/top/hihuzi/bean/RuleSimple.java
@@ -37,7 +37,7 @@ public class RuleSimple<key> implements Rule {
     @Override
     public Boolean isPermission(Object[] obj, String... args) {
 
-        return Boolean.valueOf(String.valueOf(cache.get(StrUtils.objectToString(obj)) == null ? false : cache.get(StrUtils.objectToString(obj))));
+        return Boolean.valueOf(String.valueOf(cache.get(StrUtils.objectToString(obj))));
     }
 
     @Override
